c9eab1a046386865da41a0a5f464e567e128ae35
Harryund <harryundheim@gmail.com>
2024-09-23 19:12:31 +0200

    bokstav telling

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main():
     book = []
     book = get_book(path)
     count = get_words(book)
-    print (count)
+    character_count = {}
+    character_count = count_characters(book)
+    print (character_count)
 
 def get_book(path):
     with open(path) as f:
@@ -14,6 +16,20 @@ def get_words(book):
     words = book.split()
     count = len(words)
     return count
+
+def count_characters(book):
+    character_count = {}
+    lowered_book = book.lower()
+    for letter in lowered_book:
+        if letter in character_count:
+            x = character_count.get(letter) +1
+            character_count.update({letter : x})
+        else:
+            character_count[letter] = 1
+    return character_count
+
+
+        
 
 
 main()
